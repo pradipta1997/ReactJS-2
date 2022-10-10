5c9ae395034844bc0efed753fe729be86922dfbe
Pradipta Ramadhan <pradiptaramadhan1997@gmail.com>
2022-10-10 15:15:39 +0700

    Continue upload of ReactJS, because file my-app-react cannot be opened after downloading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//HANDLE EVENT
+
+//Menggunakan Function
+// function Clicker() {
+//   function handleClick(e) {
+//     alert('Berhasil Terklik!')
+//     e.preventDefault()
+//   }
+//
+//   return (
+//       <a href="#" onClick={handleClick}>Click Here</a>
+//   )
+//
+// }
+
+//Menggunakan Class
+class Toggle extends Component {
+
+    constructor(props) {
+      super(props)
+
+      this.state = {
+          toggleStatus: true
+      }
+
+      this.handleClick = this.handleClick.bind(this)
+    }
+
+
+    handleClick() {
+        this.setState(state => ({
+            toggleStatus: !state.toggleStatus
+        }))
+    }
+
+
+    render() {
+        return(
+            <button onClick={this.handleClick}>
+                {this.state.toggleStatus ? 'ON' : 'OFF'}
+                <p>Kondisi Sekarang {this.state.toggleStatus ? 'Menyala' : 'Mati'}</p>
+            </button>
+        )
+    }
+
 }
+
+
+//METODE MENGGUNAKAN CLASS (lebih baik/recommended)
+class App extends Component{
+  render() {
+    return (
+
+      <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <Toggle />
+          </header>
+
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,41 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//INI LATIHAN (1)------
+// const name = "Pradipta Ramadhan";
+// const element = <h1>Hallo {name} !</h1>;
+//
+// ReactDOM.render(element, document.getElementById('master'));
+// -----------------
+
+
+//INI LATIHAN (2)------
+// const name = "Pradipta Ramadhan";
+//
+// function getName(){
+//
+//   return name
+// }
+//
+// const element = <h1>Hallo {getName(name)} !</h1>;
+//
+// ReactDOM.render(element, document.getElementById('master'));
+// -----------------
+
+
+//INI LATIHAN (3)------
+// const name = "BlackHaze";
+//
+// function greet(){
+//
+//   return "Hello " + name
+// }
+//
+// const element = <h1>{greet(name)} !</h1>;
+//
+// ReactDOM.render(element, document.getElementById('master'));
+// -----------------
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
